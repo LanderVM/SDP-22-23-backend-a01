d9b2beedcd8ed8b5ffe9a999e421be3fbdc457d9
LanderVM <lander.vanmolle@student.hogent.be>
2023-04-06 22:42:30 +0200

    logger and koa router/bodyparser toegevoegd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 const Koa = require("koa");
+const Router = require("@koa/router");
+const bodyParser = require("koa-bodyparser");
 const config = require("config");
+const { initializeLogger, getLogger } = require("./core/logging");
 
 const app = new Koa();
+const router = new Router();
+const NODE_ENV = process.env.NODE_ENV;
 const PORT = config.get("port");
+const LOG_LEVEL = config.get("log.level");
+const LOG_DISABLED = config.get("log.disabled");
 
-app.use(async (ctx) => {
-  ctx.body = "Hello world!";
+initializeLogger({
+  level: LOG_LEVEL,
+  disabled: LOG_DISABLED,
+  defaultMeta: { NODE_ENV },
+});
+const logger = getLogger();
+
+app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());
+
+router.get("/api", async (ctx) => {
+  ctx.body = "Hello world! v2";
 });
 
 app.listen(PORT);
-console.log(`API running on: http://localhost:${PORT}`);
+logger.info(
+  `API running on: http://localhost:${PORT}, ENV: ${NODE_ENV}, LEVEL: ${LOG_LEVEL}, DISABLED: ${LOG_DISABLED}`
+);
