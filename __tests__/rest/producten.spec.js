584fc6ac76302aefac0f10c1e01880a0d7589123
THOMAS LISSENS <THOMAS.LISSENS@student.hogent.be>
2023-04-18 11:51:44 +0200

    Product testen update

--- __tests__/rest/producten.spec.js
+++ __tests__/rest/producten.spec.js
@@ -1,5 +1,3 @@
-const config = require('config')
-
 const { tables } = require('../../src/data');
 const { withServer } = require('../helpers');
 
@@ -11,7 +9,7 @@ const data = {
         stock: 5,
         description: "omschrijning test_product 5",
         photo: "https://cloudfront.alterego-design.com/media/catalog/product/cache/6/image/9df78eab33525d08d6e5fb8d27136e95/s/p/spano_black_newsite_01_4.jpg?_gl=1*51oyrg*_ga*MTM2NTk4Mjg3OC4xNjgxMjI0MDM5*_ga_Q3T21C0ST5*MTY4MTIyNDAzOC4xLjAuMTY4MTIyNDAzOC42MC4wLjA.&_ga=2.26334013.1693298657.1681224039-1365982878.1681224039",
-        age: new Date(2023, 4, 10, 10, 10)
+        deliveryTime: "12h"
     },
     {
         product_id: 2,
@@ -20,7 +18,7 @@ const data = {
         stock: 0,
         description: "omschrijning test_product 3",
         photo: "https://cloudfront.alterego-design.com/media/catalog/product/cache/6/image/9df78eab33525d08d6e5fb8d27136e95/s/p/spano_black_newsite_01_4.jpg?_gl=1*51oyrg*_ga*MTM2NTk4Mjg3OC4xNjgxMjI0MDM5*_ga_Q3T21C0ST5*MTY4MTIyNDAzOC4xLjAuMTY4MTIyNDAzOC42MC4wLjA.&_ga=2.26334013.1693298657.1681224039-1365982878.1681224039",
-        age: new Date(2023, 4, 11, 10, 10)
+        deliveryTime: "1d"
     },
     {
         product_id: 3,
@@ -29,7 +27,7 @@ const data = {
         stock: 6,
         description: "omschrijning test_product 3",
         photo: "https://cloudfront.alterego-design.com/media/catalog/product/cache/6/image/9df78eab33525d08d6e5fb8d27136e95/s/p/spano_black_newsite_01_4.jpg?_gl=1*51oyrg*_ga*MTM2NTk4Mjg3OC4xNjgxMjI0MDM5*_ga_Q3T21C0ST5*MTY4MTIyNDAzOC4xLjAuMTY4MTIyNDAzOC42MC4wLjA.&_ga=2.26334013.1693298657.1681224039-1365982878.1681224039",
-        age: new Date(2023, 4, 12, 10, 10)
+        deliveryTime: "2d"
     }]
 }
 
@@ -62,7 +60,7 @@ describe('product', ()  => {
         it('Zou een 200 code moeten geven en alle producten', async() => {
             const response = await request.get(url)
             expect(response.status).toBe(200);
-            expect(response.body.items.length).toBe(3);
+            expect(response.body.count).toBe(3);
             expect(response.body.items[0]).toEqual({
                 product_id: 1,
 				name: "test_product vijf",
@@ -70,7 +68,7 @@ describe('product', ()  => {
 				stock: 5,
 				description: "omschrijning test_product 5",
 				photo: "https://cloudfront.alterego-design.com/media/catalog/product/cache/6/image/9df78eab33525d08d6e5fb8d27136e95/s/p/spano_black_newsite_01_4.jpg?_gl=1*51oyrg*_ga*MTM2NTk4Mjg3OC4xNjgxMjI0MDM5*_ga_Q3T21C0ST5*MTY4MTIyNDAzOC4xLjAuMTY4MTIyNDAzOC42MC4wLjA.&_ga=2.26334013.1693298657.1681224039-1365982878.1681224039",
-				age: new Date(2023, 4, 10, 10, 10).toJSON()     
+				deliveryTime: "12h"   
             });
             expect(response.body.items[1]).toEqual({
                 product_id: 2,
@@ -79,7 +77,7 @@ describe('product', ()  => {
 				stock: 0,
 				description: "omschrijning test_product 3",
 				photo: "https://cloudfront.alterego-design.com/media/catalog/product/cache/6/image/9df78eab33525d08d6e5fb8d27136e95/s/p/spano_black_newsite_01_4.jpg?_gl=1*51oyrg*_ga*MTM2NTk4Mjg3OC4xNjgxMjI0MDM5*_ga_Q3T21C0ST5*MTY4MTIyNDAzOC4xLjAuMTY4MTIyNDAzOC42MC4wLjA.&_ga=2.26334013.1693298657.1681224039-1365982878.1681224039",
-				age: new Date(2023, 4, 11, 10, 10).toJSON()         
+				deliveryTime: "1d"      
             });
             expect(response.body.items[2]).toEqual({
 				product_id: 3,
@@ -88,12 +86,12 @@ describe('product', ()  => {
 				stock: 6,
 				description: "omschrijning test_product 3",
 				photo: "https://cloudfront.alterego-design.com/media/catalog/product/cache/6/image/9df78eab33525d08d6e5fb8d27136e95/s/p/spano_black_newsite_01_4.jpg?_gl=1*51oyrg*_ga*MTM2NTk4Mjg3OC4xNjgxMjI0MDM5*_ga_Q3T21C0ST5*MTY4MTIyNDAzOC4xLjAuMTY4MTIyNDAzOC42MC4wLjA.&_ga=2.26334013.1693298657.1681224039-1365982878.1681224039",
-				age: new Date(2023, 4, 12, 10, 10).toJSON()
+				deliveryTime: "2d"
     		});
         })
 	})
 
-	describe('GET /api/product/productId/:id', () => {
+	describe('GET /api/product/:id', () => {
 
         beforeAll(async () => {
             await knex(tables.product).insert(data.producten);
@@ -104,16 +102,16 @@ describe('product', ()  => {
         });
 
         it('De response zou 200 moeten zijn en je zou een product moeten krijgen', async () => {
-            const response = await request.get(`${url}/productId/${data.producten[0].product_id}`);
+            const response = await request.get(`${url}/${data.producten[0].product_id}`);
             expect(response.status).toBe(200);
-            expect(response.body).toEqual({
+            expect(response.body.items).toEqual({
                 product_id: 1,
 				name: "test_product vijf",
 				price: 11,
 				stock: 5,
 				description: "omschrijning test_product 5",
 				photo: "https://cloudfront.alterego-design.com/media/catalog/product/cache/6/image/9df78eab33525d08d6e5fb8d27136e95/s/p/spano_black_newsite_01_4.jpg?_gl=1*51oyrg*_ga*MTM2NTk4Mjg3OC4xNjgxMjI0MDM5*_ga_Q3T21C0ST5*MTY4MTIyNDAzOC4xLjAuMTY4MTIyNDAzOC42MC4wLjA.&_ga=2.26334013.1693298657.1681224039-1365982878.1681224039",
-				age: new Date(2023, 4, 10, 10, 10).toJSON()   
+				deliveryTime: "12h"
             });
         });
     });
@@ -139,7 +137,7 @@ describe('product', ()  => {
                 stock: 6,
                 description: "omschrijning test_product 3",
                 photo: "https://cloudfront.alterego-design.com/media/catalog/product/cache/6/image/9df78eab33525d08d6e5fb8d27136e95/s/p/spano_black_newsite_01_4.jpg?_gl=1*51oyrg*_ga*MTM2NTk4Mjg3OC4xNjgxMjI0MDM5*_ga_Q3T21C0ST5*MTY4MTIyNDAzOC4xLjAuMTY4MTIyNDAzOC42MC4wLjA.&_ga=2.26334013.1693298657.1681224039-1365982878.1681224039",
-                age: new Date(2023, 4, 12, 10, 10).toJSON()  
+                deliveryTime: "2d"
             });
         });
     });
