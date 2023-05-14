138a1b8a6421d3bfaf32709f863eb3519cd6be9f
SanderGeuens <sander.geuens@student.hogent.be>
2023-05-14 16:52:27 +0200

    uitbreiden seeds voor orders, orderlines en notificaties

--- src/data/seeds/09_notification.js
+++ src/data/seeds/09_notification.js
@@ -5,7 +5,7 @@ module.exports = {
     await knex(tables.order_notification).insert([
       {
         notification_id:1,
-        order_date: new Date(),
+        order_date: new Date(2023,05,03),
         CUSTOMER_supplier_id:1,
         ORDER_order_id:2,
       },{
@@ -13,6 +13,41 @@ module.exports = {
         order_date: new Date(),
         CUSTOMER_supplier_id:2,
         ORDER_order_id:4,
+      },{
+        notification_id:3,
+        order_date: new Date(),
+        CUSTOMER_supplier_id:2,
+        ORDER_order_id:5,
+      },{
+        notification_id:4,
+        order_date: new Date(),
+        CUSTOMER_supplier_id:2,
+        ORDER_order_id:6,
+      },{
+        notification_id:5,
+        order_date: new Date(),
+        CUSTOMER_supplier_id:2,
+        ORDER_order_id:7,
+      },{
+        notification_id:6,
+        order_date: new Date(),
+        CUSTOMER_supplier_id:2,
+        ORDER_order_id:8,
+      },{
+        notification_id:7,
+        order_date: new Date(),
+        CUSTOMER_supplier_id:2,
+        ORDER_order_id:9,
+      },{
+        notification_id:8,
+        order_date: new Date(),
+        CUSTOMER_supplier_id:2,
+        ORDER_order_id:10,
+      },{
+        notification_id:9,
+        order_date: new Date(),
+        CUSTOMER_supplier_id:2,
+        ORDER_order_id:11,
       },
     ]);
   },
