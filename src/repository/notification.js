7feaae46db6722ffa9ac18bc7ff77515ef35800e
SanderGeuens <sander.geuens@student.hogent.be>
2023-05-17 00:01:57 +0200

    aanpassen repository notificaties

--- src/repository/notification.js
+++ src/repository/notification.js
@@ -4,8 +4,10 @@ const getAllByAuthId = async (auth0Id) =>{
   console.log(auth0Id);
   const supplierId = await getKnex()(tables.customer).select("SUPPLIER_supplier_id").where("auth0_id", auth0Id);
   
-  const notifications = await getKnex()(tables.order_notification).select("*")
-  .where("CUSTOMER_supplier_id",supplierId[0].SUPPLIER_supplier_id.toString());
+  const notifications = await getKnex()(tables.order_notification)
+  .join(tables.order,`${tables.order_notification}.ORDER_order_id`,"=",`${tables.order}.order_id`)
+  .select("*")
+  .where(`${tables.order_notification}.CUSTOMER_supplier_id`,supplierId[0].SUPPLIER_supplier_id.toString());
   
   return notifications;
 }
