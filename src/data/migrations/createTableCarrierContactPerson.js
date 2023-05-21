508106ba63e81aa4d3b62ae19bff56bd9541464f
unknown <lander.vanmolle@student.hogent.be>
2023-05-21 22:24:20 +0200

    bugfix foutive tabel namen

--- src/data/migrations/createTableCarrierContactPerson.js
+++ src/data/migrations/createTableCarrierContactPerson.js
@@ -4,21 +4,27 @@ module.exports = {
   up: async (knex) => {
     await knex.schema.createTable(tables.carrier_contact_person, (table) => {
       table
-        .integer("carrier_id")
+        .integer("Carrier_carrier_id")
         .unsigned()
         .notNullable()
         .references(`${tables.carrier}.carrier_id`)
         .onDelete("CASCADE")
-        .onUpdate("CASCADE");
+        .onUpdate("CASCADE")
+        .withKeyName("fk_carrier_contact_person_carrier");
+
       table
-        .integer("contact_person_id")
+        .integer("contactPersonList_contact_person_id")
         .unsigned()
         .notNullable()
         .references(`${tables.contact_person}.contact_person_id`)
         .onDelete("CASCADE")
-        .onUpdate("CASCADE");
+        .onUpdate("CASCADE")
+        .withKeyName("fk_carrier_contact_person_contact_person");
 
-      table.primary(["carrier_id", "contact_person_id"]);
+      table.primary([
+        "Carrier_carrier_id",
+        "contactPersonList_contact_person_id",
+      ]);
     });
   },
   down: (knex) => {
--- src/data/seeds/carrierContactPerson.js
+++ src/data/seeds/carrierContactPerson.js
@@ -4,28 +4,28 @@ module.exports = {
   seed: async (knex) => {
     await knex(tables.carrier_contact_person).insert([
       {
-        carrier_id: 1,
-        contact_person_id: 4,
+        Carrier_carrier_id: 1,
+        contactPersonList_contact_person_id: 4,
       },
       {
-        carrier_id: 2,
-        contact_person_id: 5,
+        Carrier_carrier_id: 2,
+        contactPersonList_contact_person_id: 5,
       },
       {
-        carrier_id: 2,
-        contact_person_id: 6,
+        Carrier_carrier_id: 2,
+        contactPersonList_contact_person_id: 6,
       },
       {
-        carrier_id: 3,
-        contact_person_id: 2,
+        Carrier_carrier_id: 3,
+        contactPersonList_contact_person_id: 2,
       },
       {
-        carrier_id: 3,
-        contact_person_id: 3,
+        Carrier_carrier_id: 3,
+        contactPersonList_contact_person_id: 3,
       },
       {
-        carrier_id: 4,
-        contact_person_id: 1,
+        Carrier_carrier_id: 4,
+        contactPersonList_contact_person_id: 1,
       },
     ]);
   },
