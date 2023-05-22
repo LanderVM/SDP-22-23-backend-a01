19ba247b1db862772a001d8ae60ad5f4eb7121db
unknown <lander.vanmolle@student.hogent.be>
2023-05-23 00:35:59 +0200

    typo contactperson supplier_id

--- src/data/seeds/supplierContactPerson.js
+++ src/data/seeds/supplierContactPerson.js
@@ -6,22 +6,22 @@ module.exports = {
       {
         email_id: "bert.bratwurst@gmail.com",
         full_name: "Bert Bratwurst",
-        SUPPLIER_supplier_id: 3,
+        SUPPLIER_supplier_id: 1,
       },
       {
         email_id: "elke.daems@gmail.com",
         full_name: "Elke Daems",
-        SUPPLIER_supplier_id: 3,
+        SUPPLIER_supplier_id: 1,
       },
       {
         email_id: "erik.tanner@gmail.com",
         full_name: "Erik Tanner",
-        SUPPLIER_supplier_id: 3,
+        SUPPLIER_supplier_id: 2,
       },
       {
         email_id: "jan.jaap@gmail.com",
         full_name: "Jan Jaap",
-        SUPPLIER_supplier_id: 3,
+        SUPPLIER_supplier_id: 2,
       },
     ]);
   },
