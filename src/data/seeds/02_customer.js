f3f3488b7918afa51113d88604599853f2f69933
Maurice Cantaert <maurice.cantaert@student.hogent.be>
2023-06-19 00:52:22 +0200

    fix bert timco user seed

--- src/data/seeds/02_customer.js
+++ src/data/seeds/02_customer.js
@@ -35,8 +35,8 @@ module.exports = {
       },
       {
         id: 4,
-        auth0_id: "auth0|64439bc8bc6509196a8e5990",
-        email: "bert@timCo.com",
+        auth0_id: "auth0|648f79553cedf9f3adc7c59c",
+        email: "berttimco@gmail.com",
         username: "Bert",
         image_url:
           "https://s.gravatar.com/avatar/74d81d6aada690a1b114800a2c3dcec8?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fbe.png",
