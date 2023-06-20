d27ffe414fd68078b68f153f6e562de55be10042
unknown <lander.vanmolle@student.hogent.be>
2023-06-20 16:50:21 +0200

    corrected seeding

--- src/data/seeds/04_contactPerson.js
+++ src/data/seeds/04_contactPerson.js
@@ -5,33 +5,33 @@ module.exports = {
     await knex(tables.contact_person).insert([
       {
         contact_person_id: 1,
-        email: "twee@post.nl",
-        phone_number: "899321480",
+        email: "BramDeVries@post.nl",
+        phone_number: "0491 87 65 43",
       },
       {
         contact_person_id: 2,
-        email: "vier@bpost.be",
-        phone_number: "479554433",
+        email: "ThijsJansen@bpost.be",
+        phone_number: "0479 12 34 56",
       },
       {
         contact_person_id: 3,
-        email: "drie@bpost.be",
-        phone_number: "499334455",
+        email: "EvaBakker@bpost.be",
+        phone_number: "0499 33 44 55",
       },
       {
         contact_person_id: 4,
-        email: "een@post.nl",
-        phone_number: "899321480",
+        email: "LucasJacobs@post.nl",
+        phone_number: "0493 23 45 67",
       },
       {
         contact_person_id: 5,
-        email: "een@bpost.be",
-        phone_number: "499334455",
+        email: "SaraMulder@bpost.be",
+        phone_number: "0474 87 65 43",
       },
       {
         contact_person_id: 6,
-        email: "twee@bpost.be",
-        phone_number: "479554433",
+        email: "DaanVanDerLinden@bpost.be",
+        phone_number: "0479 55 44 33",
       },
     ]);
   },
